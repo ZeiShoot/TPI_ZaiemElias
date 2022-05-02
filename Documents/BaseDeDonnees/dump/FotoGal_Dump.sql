1f9e064e2c285115aca2a2a06779c161192bf093
Elias Zaiem <elias.zaiem16@gmail.com>
2022-05-02 13:47:33 +0200

    Documentation, Planning, Base de données
    Le planning est terminé, j'ai aussi finalisé la base de données et fait un Dump de celle-ci

---
+++ Documents/BaseDeDonnees/dump/FotoGal_Dump.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: FotoGal
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.3.34-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `like_unlike`
+--
+
+DROP TABLE IF EXISTS `like_unlike`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `like_unlike` (
+  `like` tinyint(4) NOT NULL,
+  `date` datetime(6) NOT NULL,
+  `utilisateurs_idUser` int(11) NOT NULL,
+  `production_idProduction` int(11) NOT NULL,
+  KEY `like_unlike_FK` (`utilisateurs_idUser`),
+  KEY `like_unlike_FK_1` (`production_idProduction`),
+  CONSTRAINT `like_unlike_FK` FOREIGN KEY (`utilisateurs_idUser`) REFERENCES `utilisateurs` (`idUser`),
+  CONSTRAINT `like_unlike_FK_1` FOREIGN KEY (`production_idProduction`) REFERENCES `productions` (`idProduction`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `like_unlike`
+--
+
+LOCK TABLES `like_unlike` WRITE;
+/*!40000 ALTER TABLE `like_unlike` DISABLE KEYS */;
+/*!40000 ALTER TABLE `like_unlike` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productions`
+--
+
+DROP TABLE IF EXISTS `productions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productions` (
+  `idProduction` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(45) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `date_soumission` datetime(6) NOT NULL,
+  `date_modification` datetime(6) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `categories_idCategorie` int(11) NOT NULL,
+  `utilisateurs_idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idProduction`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productions`
+--
+
+LOCK TABLES `productions` WRITE;
+/*!40000 ALTER TABLE `productions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateurs` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` char(128) NOT NULL,
+  `_est_admin` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+LOCK TABLES `utilisateurs` WRITE;
+/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'FotoGal'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-02 13:46:04
